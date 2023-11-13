102f200d80bfff715b463ab3bc69bc7931b317ba
Hansi P <hansipie@users.noreply.github.com>
2023-11-13 11:43:57 +0200

    fix assistant file delete
    + add files table en delete button
    + toast messages

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ import os
 import streamlit as st
 import pandas as pd
 from openai import OpenAI
+import os
 
 st.set_page_config(layout="wide")
 st.title('Assistants Management')
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="open_api_key", type="password")
+    if os.environ.get('OPENAI_API_KEY'):
+        print("Using OpenAI API key from environment variable")
+        openai_api_key = os.environ.get('OPENAI_API_KEY')
+    else:
+        print("Using OpenAI API key from user input")
+        openai_api_key = st.text_input("OpenAI API Key", key="open_api_key", type="password")
     if (st.button('Reload')):
         st.rerun()
 
@@ -16,6 +22,15 @@ if not openai_api_key:
     st.stop()
 
 client=OpenAI(api_key=openai_api_key)
+
+def toastMessage(message):
+    if message.deleted:
+        st.toast(f"Delete {message.object} {message.id}")  
+    else:
+        st.toast(f"Error: delete {message.object} {message.id} failed")
+
+
+### Assistants ###
 
 my_assistants = client.beta.assistants.list()
 
@@ -45,10 +60,45 @@ if (st.button('Delete selected assistants')):
     for index, row in edited_df.iterrows():
         print(row['selected'], row['name'], row['id'])
         if row['selected']:
-            st.write("delete: ", row['name'], row['id'])
             for file_id in row['file_ids']:
-                response_delfiles = client.beta.assistants.files.delete(assistant_id=row['id'], file_id=file_id)
+                #response_delfiles = client.beta.assistants.files.delete(assistant_id=row['id'], file_id=file_id) # not working
+                response_delfiles = client.files.delete(file_id)
                 print("delete file:", response_delfiles)
+                # print type of json variable
+                toastMessage(response_delfiles)
             response_delassist = client.beta.assistants.delete(row['id'])
             print("delete assistant:", response_delassist)
+            toastMessage(response_delassist)
+    st.rerun()
+
+### Files ###
+
+my_files = client.files.list()
+df_files = pd.DataFrame(my_files.data, columns=['id', 'bytes', 'created_at', 'filename', 'object', 'purpose', 'status', 'status_details'])
+df_files = df_files.map(lambda x: x[1] if isinstance(x, tuple) else str(x))
+
+# df formating
+df_files['created_at'] = pd.to_datetime(df_files['created_at'], unit='s')
+
+# add columns for selection tickboxes
+df_files['selected'] = False
+
+# show the dataframe
+edited_df_file = st.data_editor(df_files[['selected', 'filename', 'bytes', 'created_at', 'purpose', 'id']], column_config={
+        "selected": st.column_config.CheckboxColumn(
+            "Select",
+            help="Select file for deletion",
+            default=False,
+        )
+    },
+    disabled=['filename', 'bytes', 'created_at', 'purpose', 'id'],
+    hide_index=True)
+
+if (st.button('Delete selected files')):
+    for index, row in edited_df_file.iterrows():
+        print(row['selected'], row['filename'], row['id'])
+        if row['selected']:
+            response_delfile = client.files.delete(row['id'])
+            print("delete file:", response_delfile)
+            toastMessage(response_delfile)
     st.rerun()
